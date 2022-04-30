5b83df25f14c91206cf7ed13176a9b705fdabb86
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-04-30 22:55:24 +0400

    Added the signup page

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 3000
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 
-app.get("/", (req, res) => res.send("Hey"))
+app.get("/", (req, res) => res.render("signup"))
 
 app.listen(PORT, () => console.log("current listening at port " + PORT))
