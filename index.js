ab2069cd6eda26f1c7bbbfea736952f3ee8e07ce
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-04-30 22:50:52 +0400

    make it better

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+app.get("/", (req, res) => res.send("Hey"))
+
+app.listen(PORT, () => console.log("current listening at port " + PORT))
